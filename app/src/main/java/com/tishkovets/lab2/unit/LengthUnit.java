eb45d2109c33ceb3287fbf77f7a8b395798c28ce
Elizaveta Tishkovets <tiishka2@gmail.com>
2021-10-23 12:19:27 +0300

    Исправлены ошибки в единицах длин

--- app/src/main/java/com/tishkovets/lab2/unit/LengthUnit.java
+++ app/src/main/java/com/tishkovets/lab2/unit/LengthUnit.java
@@ -4,9 +4,9 @@ import com.tishkovets.lab2.R;
 
 public enum LengthUnit implements UnitType {
     METER(1, R.plurals.meters_plurals),
-    INCH(2.205, R.plurals.inches_plurals),
-    FOOT(35.274, R.plurals.feet_plurals),
-    YARD(0.1574, R.plurals.yards_plurals);
+    INCH(39.37, R.plurals.inches_plurals),
+    FOOT(3.281, R.plurals.feet_plurals),
+    YARD(1.094, R.plurals.yards_plurals);
 
     private final double koeff;
     private final int plural;
